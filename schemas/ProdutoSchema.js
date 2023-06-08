7df3e23890d70eae7c4ce0a723c35f98b8ae5099
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2023-06-08 19:03:45 -0300

    Correção de erro no ProdutoSchema

--- schemas/ProdutoSchema.js
+++ schemas/ProdutoSchema.js
@@ -1,7 +1,27 @@
 const { Schema, model } = require('mongoose');
 
 const ProdutoSchema = new Schema({
-  description: String,
+  nome: {
+    type: String,
+    required: true,
+  },
+  valor: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fornecedor: {
+    type: String,
+    required: true,
+  },
+  imagemUrl: {
+    type: String,
+    required: true,
+  },
+  descricao: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = model('Produto', ProdutoSchema);
