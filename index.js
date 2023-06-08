4bc3579869da37d0e25f341ba731b2e1cbd36625
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2023-06-08 16:19:49 -0300

    Configuração do Cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const ProdutoSchema = require('./schemas/ProdutoSchema');
 
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3333;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(
